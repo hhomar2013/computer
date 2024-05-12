6f8adb21b5a3f84f5d4b1381433198687bbe6eed
hhomar2013 <hhomar2013@gmail.com>
2024-05-12 11:38:01 +0300

    Change page title

--- motherboard_power.php
+++ motherboard_power.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>ألكتروهيلب</title>
+  <title>ألكتروهيلب | اللوحة الأم و الطاقة</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
